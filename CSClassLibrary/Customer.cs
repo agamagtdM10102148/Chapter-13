15266712703adbaac72200cf53592e776fe44c60
yinengwang <agamagtd@gmail.com>
2018-06-30 09:41:48 +0800

    Add project files.

---
+++ CSClassLibrary/Customer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSClassLibrary
+{
+    public class Customer
+    {
+        public Customer()
+        {
+            Orders = new List<Order>();
+        }
+
+        public string CompanyName { get; set; }
+        public string City { get; set; }
+
+        public List<Order> Orders { get; private set; }
+    }
+}
---
+++ CSClassLibrary/Order.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSClassLibrary
+{
+    public class Order
+    {
+        public string ProductId { get; set; }
+        public string Description { get; set; }
+    }
+}
